6e43bbac63a13ae8fc38e50c5663eaf9198b25e0
ruthvik125 <ruthvik.mitmpl2022@learner.manipal.edu>
2024-08-10 06:41:17 +0530

    added dummy problems in problemsTable.js
    

--- server/routes/problemsTable.js
+++ server/routes/problemsTable.js
@@ -1,5 +1,21 @@
 import Problems from "../models/ProblemsTable.js";
 
+const problems = [
+  { order: 1, title: "Two Sum", difficult: "Easy", category: "Array" },
+  { order: 2, title: "Longest Substring Without Repeating Characters", difficult: "Medium", category: "String" },
+  { order: 3, title: "Median of Two Sorted Arrays", difficult: "Hard", category: "Array" },
+  { order: 4, title: "Valid Parentheses", difficult: "Easy", category: "Stack" },
+  { order: 5, title: "Merge Intervals", difficult: "Medium", category: "Sorting" },
+];
+
+Problems.insertMany(problems)
+  .then(() => {
+    console.log("Data inserted");
+  })
+  .catch((err) => {
+    console.error("Error inserting data", err);
+  });
+
 export const problemsTable = async (req, res) => {
   try {
     const problems = await Problems.find().select(
